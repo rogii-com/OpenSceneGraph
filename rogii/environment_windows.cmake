dcd11614994ea005709d0ebc3c3bb5031d21f85c
Artem Muralev <a.muralev@rogii.com>
2025-03-19 15:45:06 +0400

    STARSTEER-19558 Adopt for own ANGLE

--- rogii/environment_windows.cmake
+++ rogii/environment_windows.cmake
@@ -1,15 +1,15 @@
 include(${CMAKE_CURRENT_LIST_DIR}/msvs_package.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/windowssdk_package.cmake)
 
-if(OSG_QT_ANGLE)
+if(OSG_USE_SS_OWN_ANGLE)
     CNPM_ADD_PACKAGE(
         NAME
-            Qt
+            angle
         VERSION
-            5.15.1
+            6778.0.0
         BUILD_NUMBER
-            1
+            13
         TAG
-            "sdk18362_vsbt19"
+            "sdk22621_vsbt22"
     )
 endif()
